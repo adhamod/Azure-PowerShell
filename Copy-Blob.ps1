6a1beefba32491a5070bf8f92d6ea2ec98b091ad
Carlos Patiño <cap273@cornell.edu>
2015-09-21 17:45:22 -0400

    Minor Changes

--- Copy-Blob.ps1
+++ Copy-Blob.ps1
@@ -1,7 +1,10 @@
 ﻿<#
-Starts a copy operation of a specific blob. This script works for both ARM and ASM.
+This script copies a blob using user-inputted connection strings for the Source and the
+Destination storage accounts. Can be used to copy VHD files across storage accounts.
 
-User input is required.
+This script has been tested to function correctly in both ASM and ARM.
+
+Author: Carlos Patiño, carpat@microsoft.com
 #>
 
 
@@ -10,21 +13,21 @@ User input is required.
 ##################################
 
 # Connection String for the SOURCE storage account
-$srcConnectionString = "DefaultEndpointsProtocol=https;AccountName=testresourcegroup16710;AccountKey=xxx"
+$srcConnectionString = "DefaultEndpointsProtocol=https;AccountName=xxx;AccountKey=xxx"
 
 # Container name in which the SOURCE file is located (container must be inside SOURCE storage account)
-$srcContainerName = "vhds"
+$srcContainerName = "testContainer"
 
 # Blob name of the SOURCE file (must be located inside SOURCE container)
-$srcBlobName = "testVM-SV3-2.vhd"
+$srcBlobName = "testBlob.vhd"
 
 ###
 
 # Connection String for the DESTINATION storage account
-$destConnectionString = "DefaultEndpointsProtocol=https;AccountName=teststore123456;AccountKey=xxx"
+$destConnectionString = "DefaultEndpointsProtocol=https;AccountName=xxx;AccountKey=xxx"
 
 # Container name in which the DESTINATION file will be located (container must be inside DESTINATION storage account)
-$destContainerName = "testblob"
+$destContainerName = "testDestinationContainer"
 
 ##################################
 # END OF REQUIRED USER INPUT
