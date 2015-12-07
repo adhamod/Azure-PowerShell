494924848f43a657cbf66748d8a3878b4d620216
Carlos Patiño <cap273@cornell.edu>
2015-12-07 17:41:50 -0600

    Major update for SQL Server installation file.

--- Install-SQLServer.ps1
+++ Install-SQLServer.ps1
@@ -23,7 +23,13 @@
 
 .PARAMETER sqlInstallationPath
 	Path of the SQL Server installation file (i.e. Setup.exe)
+
+.PARAMETER sizeTempDBDataFileMB
+	The initial configured size of the TempDB data files, in MB.
 	
+.PARAMETER percentAutogrowTempDB
+	The file growth of the TempDB data files, as a percentage.
+
 .EXAMPLE
     Install-SQLServer -ServerName testVM1 -LocalAdmin charliebrown -DotNet35SourcePath C:\Downloads\sources\sxs -sqlInstallationPath "C:\Downloads\SQLFULL_x64_ENU\Setup.exe"
 
@@ -40,11 +46,19 @@ param (
     [Parameter(Mandatory=$false)]
     [ValidateNotNullOrEmpty()]
     [String]
-    $DotNet35SourcePath = "C:\Users\TestAdmin\Downloads\dotnet35source\sxs",
+    $DotNet35SourcePath = "C:\Users\charliebrown\Downloads\dotnet35source\sxs",
 
     [ValidateNotNullOrEmpty()]
     [String]
-    $sqlInstallationPath = "C:\Users\TestAdmin\Downloads\SQLServer2014\Setup.exe"
+    $sqlInstallationPath = "C:\Users\charliebrown\Downloads\SQLServer2014\Setup.exe",
+
+    [ValidateNotNullOrEmpty()]
+    [int]
+    $sizeTempDBDataFileMB = 256,
+
+    [ValidateNotNullOrEmpty()]
+    [int]
+    $percentAutogrowTempDB = 10
 
     )
 
@@ -55,32 +69,90 @@ param (
 $ServerName = $env:COMPUTERNAME # Name of the local computer.
 $LocalAdmin = "TestAdmin" # Name of the current user (which in our particular case is always going to be the local administrator)
 
-
+# It appears that Try/Catch/Finally and Trap in PowerShell only works with terminating errors.
+# Make all errors terminating
+$ErrorActionPreference = "Stop"; 
 
 
 ########################################
 # Set up Storage Pool and Virtual Disk
 ########################################
+<# Overarching logic:
+    Skip this entire section if the F: drive already exists.
+    If it does not exist, create a new Storage Pool, a Virtual Disk, and a Volume with Drive Letter F:
+#>
 
-# Gets the storage subsystem object for the Storage Spaces subsystem, passes it to the 
-# Get-PhysicalDisk cmdlet, which then gets the physical disks in the specified subsystem that are available to add to a 
-# storage pool
-$PhysicalDisks = Get-StorageSubSystem -FriendlyName "Storage Spaces*" | Get-PhysicalDisk -CanPool $True
+# Check for existence of current Storage Pools. Do not attept to create a Storage Pool if a 
+# non-Primordial Storage Pool already exists
+$DriveFexists = $false
+foreach ($volume in (Get-Volume)) {
+    if ($volume.DriveLetter -eq 'F') {
+        $DriveFexists = $true
+    }
+}
 
-#This line creates a new storage pool using the $PhysicalDisks variable to specify the disks to include from the Storage 
-#Spaces subsystem (specified with a wildcard * to remove the need to modify the friendly name for different computers).
-New-StoragePool -FriendlyName DataDiskStoragePool `
-    -StorageSubsystemFriendlyName "Storage Spaces*" `
-    -PhysicalDisks $PhysicalDisks `
-    -ResiliencySettingNameDefault Simple `
-    -ProvisioningTypeDefault Fixed `
-    -ErrorAction Stop
+if ($DriveFexists) {
+    Write-Host "
+                Drive F: already exists in the target machine.
+                Creation of a Storage Pool, a new Virtual Disk, and a new Volume, will be skipped.
+                Please ensure that Drive F is configured appropriately for SQL Server."
+}
+else {
 
-# Create new virtual disk, then Initialize it, then make a new partition on the F drive.
-New-VirtualDisk -StoragePoolFriendlyName DataDiskStoragePool -FriendlyName "VirtualDataDisk" `
-            -UseMaximumSize -ProvisioningType Fixed | Initialize-Disk  -PassThru | New-Partition -DriveLetter 'F' -UseMaximumSize `
-            | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS -Confirm:$false -Force -ErrorAction Stop
+    try
+    {
+    
+        # Gets the storage subsystem object for the Storage Spaces subsystem, passes it to the 
+        # Get-PhysicalDisk cmdlet, which then gets the physical disks in the specified subsystem that are 
+        # available to add to a storage pool
+        $PhysicalDisks = Get-StorageSubSystem -FriendlyName "Storage Spaces*" | Get-PhysicalDisk -CanPool $True
 
+    }
+    catch [Microsoft.PowerShell.Cmdletization.Cim.CimJobException]
+    {
+        throw "
+                Error: There are no physical disks available to add to a new Storage Pool. 
+                Please check that the data disks have been successfully attached to the VM."
+    }
+
+    # Check for existence of current Storage Pools. Do not attept to create a Storage Pool if a 
+    # non-Primordial Storage Pool already exists
+    foreach ($storagePool in (Get-StoragePool)) {
+        if (  !($storagePool.IsPrimordial)  ) {
+            throw "
+                    Error: A non-primordial Storage Pool already exists.
+                    This script is intended to run on a newly-provisioned VM, and may fail if
+                    a Storage Pool has already exists in the target machine."
+        }
+    }
+
+    <# 
+       Create a new storage pool using the $PhysicalDisks variable to specify the disks to include 
+       from the Storage Spaces subsystem (specified with a wildcard * to remove the need to modify the 
+       friendly name for different computers).
+    #>
+    New-StoragePool -FriendlyName DataDiskStoragePool `
+        -StorageSubsystemFriendlyName "Storage Spaces*" `
+        -PhysicalDisks $PhysicalDisks `
+        -ResiliencySettingNameDefault Simple `
+        -ProvisioningTypeDefault Fixed | Out-Null
+
+    # Stops the Hardware Detection Service to prevent the Format Disk prompt window from popping up
+    Stop-Service -Name ShellHWDetection
+
+    # Create new virtual disk, then Initialize it, then make a new partition on the F drive.
+    New-VirtualDisk -StoragePoolFriendlyName DataDiskStoragePool -FriendlyName "VirtualDataDisk" `
+            -UseMaximumSize -ProvisioningType Fixed `
+            | Initialize-Disk  -PassThru `
+            | New-Partition -DriveLetter 'F' -UseMaximumSize `
+            | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS -Confirm:$false -Force | Out-Null
+
+    # Starts the Hardware Detection Service again
+    Start-Service -Name ShellHWDetection
+
+    Write-Host "A new volume on Drive F: has successfully been created."
+
+}
 
 
 
@@ -88,12 +160,12 @@ New-VirtualDisk -StoragePoolFriendlyName DataDiskStoragePool -FriendlyName "Virt
 # Install .NET Framework 3.5
 ########################################
 
-Install-WindowsFeature -Name Net-Framework-Core -source $DotNet35SourcePath -ErrorAction Stop
+Install-WindowsFeature -Name Net-Framework-Core -source $DotNet35SourcePath | Out-Null
+
+Write-Host ".NET Framework 3.5 successfully installed."
 
 # Areas for improvement: reference this article:
 # http://stackoverflow.com/questions/303045/connecting-to-a-network-folder-with-username-password-in-powershell
-
-
 
 
 ########################################
@@ -101,10 +173,10 @@ Install-WindowsFeature -Name Net-Framework-Core -source $DotNet35SourcePath -Err
 ########################################
 
 # Create new directories
-New-Item -ItemType directory -Path F:\SQL_Data | Out-Null
-New-Item -ItemType directory -Path F:\SQL_Backup | Out-Null
-New-Item -ItemType directory -Path F:\SQL_Logs | Out-Null
-New-Item -ItemType directory -Path F:\SQL_TempDB | Out-Null
+New-Item -ItemType directory -Path F:\SQL_Data     | Out-Null
+New-Item -ItemType directory -Path F:\SQL_Backup   | Out-Null
+New-Item -ItemType directory -Path F:\SQL_Logs     | Out-Null
+New-Item -ItemType directory -Path F:\SQL_TempDB   | Out-Null
 
 # Open up firewall ports
 # For Database Engine default instance
@@ -147,24 +219,51 @@ $myArgList += '/SAPWD=W3lc0me0 '
 $myArgList += '/TCPENABLED=1'                                      # Enable TCP/IP Protocol
 
 # Start the installation process with the specified parameters.
-Start-Process -Verb runas -FilePath $sqlInstallationPath -ArgumentList $myArgList -Wait -ErrorAction Stop
+Start-Process -Verb runas -FilePath $sqlInstallationPath -ArgumentList $myArgList -Wait
 
-
+Write-Host "SQL Server successfully installed."
 
 
 ########################################
 # SQL Server Post-Installation
 ########################################
 
-# Create SQL Query to move the TEMPDB database from its default location to the F: drive
-# Using this guide: http://stackoverflow.com/questions/8423541/how-do-you-run-a-sql-server-query-from-powershell
+<#
+    Create SQL Query to move the TEMPDB database from its default location to the F: drive
+    Using this guide: http://stackoverflow.com/questions/8423541/how-do-you-run-a-sql-server-query-from-powershell
+
+    Also, for TempDB, create 8 data files (as opposed to the default of 1).
+    Using this as a Best Practices source: http://www.brentozar.com/sql/tempdb-performance-and-configuration/
+    
+#>
 $DatabaseName = "master"
 $Query = "
           ALTER DATABASE tempdb
-          MODIFY FILE (name = tempdev, FILENAME = 'F:\SQL_TempDB\tempdb.mdf');
+          MODIFY FILE (name = tempdev, FILENAME = 'F:\SQL_TempDB\tempdb.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
           
           ALTER DATABASE tempdb
           MODIFY FILE (name = templog, FILENAME = 'F:\SQL_TempDB\templog.ldf');
+
+          ALTER DATABASE tempdb
+          ADD FILE (NAME = tempdev2, FILENAME = 'F:\SQL_TempDB\tempdb2.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+
+          ALTER DATABASE tempdb
+          ADD FILE (NAME = tempdev3, FILENAME = 'F:\SQL_TempDB\tempdb3.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+
+          ALTER DATABASE tempdb
+          ADD FILE (NAME = tempdev4, FILENAME = 'F:\SQL_TempDB\tempdb4.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+
+          ALTER DATABASE tempdb
+          ADD FILE (NAME = tempdev5, FILENAME = 'F:\SQL_TempDB\tempdb5.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+
+          ALTER DATABASE tempdb
+          ADD FILE (NAME = tempdev6, FILENAME = 'F:\SQL_TempDB\tempdb6.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+
+          ALTER DATABASE tempdb
+          ADD FILE (NAME = tempdev7, FILENAME = 'F:\SQL_TempDB\tempdb7.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+
+          ALTER DATABASE tempdb
+          ADD FILE (NAME = tempdev8, FILENAME = 'F:\SQL_TempDB\tempdb8.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
          "
 
 # Timeout parameters
@@ -200,3 +299,5 @@ $ds.Tables
 
 # Restart the SQL Server instance
 Restart-Service -Name 'MSSQLSERVER' -Force
+
+Write-Host "SQL Server successfully configured."
