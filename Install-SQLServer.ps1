d5006d5451d0fbdd5fe6f27a2f8361251ebb40a8
Carlos Patiño <cap273@cornell.edu>
2016-01-12 23:19:53 -0600

    Major updates to SQL installation script

--- Install-SQLServer.ps1
+++ Install-SQLServer.ps1
@@ -10,13 +10,11 @@
 	
 	This script performs the following operations:
         - Create a storage pool from the available data disks attached to the host.
-        - Create a Virtual Disk on the F: drive from the storage pool, 
-            with an allocation unit size of 64KB (i.e. 65,536 bytes)
+        - Create a Virtual Disk from the storage pool.
+        - Create a number of volues with an allocation unit size of 64KB (i.e. 65,536 bytes)
         - Install .NET Framework 3.5 from a source file.
-        - Open ports 1433 (for the SQL Server Engine) and port 2383 (for Analysis Services) on the firewall.
-        - Install SQL Server from a source file.
-        - Move the tempdb database to the F: drive.
-        - Move the default locations for the Data, Backup, and Log databases to the F: drive.
+        - Open ports 1433 (for the SQL Server Engine) and port 59999 (for ILB Probe Port) on the firewall.
+        - Install SQL Server from a source file, with certain volume configurations.
 
 .PARAMETER DotNet35SourcePath
 	Path of the .NET 3.5 installation folder (i.e. the \sources\sxs folder)
@@ -27,18 +25,12 @@
 .PARAMETER sizeTempDBDataFileMB
 	The initial configured size of the TempDB data files, in MB.
 	
-.PARAMETER percentAutogrowTempDB
-	The file growth of the TempDB data files, as a percentage.
-
-.PARAMETER LocalAdmin
-	Name of the Local Administrator of the machine in which this script will run.
-
-.EXAMPLE
-    Install-SQLServer -DotNet35SourcePath C:\Downloads\sources\sxs -sqlInstallationPath "C:\Downloads\SQLFULL_x64_ENU\Setup.exe" -sizeTempDBDataFileMB 256 -percentAutogrowTempDB 10
+.PARAMETER autogrowTempDBinMB
+	The file growth of the TempDB data files, in MB.
 
 .NOTES
     AUTHOR: Carlos Patiño
-    LASTEDIT: November 15 Dec 2015
+    LASTEDIT: January 12 2016
 
 #>
 
@@ -50,15 +42,15 @@ param (
 
     [ValidateNotNullOrEmpty()]
     [String]
-    $sqlInstallationPath = "\\ps11170644tou02.cloud.wal-mart.com\Source\SQLServer2012\Setup.exe",
+    $sqlInstallationPath = "\\ps11170644tou02.cloud.wal-mart.com\Source\SQLServer2014\Setup.exe",
 
     [ValidateNotNullOrEmpty()]
     [int]
-    $sizeTempDBDataFileMB = 256,
+    $sizeTempDBDataFileMB = 5000,
 
     [ValidateNotNullOrEmpty()]
     [int]
-    $percentAutogrowTempDB = 10,
+    $autogrowTempDBinMB = 500,
 
     [ValidateNotNullOrEmpty()]
     [string]
@@ -143,20 +135,87 @@ else {
     # Stops the Hardware Detection Service to prevent the Format Disk prompt window from popping up
     Stop-Service -Name ShellHWDetection
 
-    # Create new virtual disk, then Initialize it, then make a new partition on the F drive.
+    # Create new virtual disk, then Initialize it.
     New-VirtualDisk -StoragePoolFriendlyName DataDiskStoragePool -FriendlyName "VirtualDataDisk" `
             -UseMaximumSize -ProvisioningType Fixed `
-            | Initialize-Disk  -PassThru `
-            | New-Partition -DriveLetter 'F' -UseMaximumSize `
-            | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS -Confirm:$false -Force | Out-Null
+            | Initialize-Disk  -PassThru
+
+    $VirtualDisk = Get-Disk -FriendlyName "Microsoft Storage Space Device" | Out-Null
+
+    <#
+        Make partitions on the disk according to SQL Server standard
+    #>
+
+    # E drive
+    try 
+    {
+        $VirtualDisk `
+                | New-Partition -DriveLetter 'E' -Size (50GB) `
+                | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS `
+                                -NewFileSystemLabel "SQLSys" -Confirm:$false -Force | Out-Null
+    }
+    
+    catch
+    {
+        $drv = Get-WmiObject win32_volume -filter 'DriveLetter = "E:"'
+        $drv.DriveLetter = "Z:"
+        $drv.Put() | Out-Null
+
+        # Try again after reassigning DVD drive to Z: drive
+        $VirtualDisk `
+                | New-Partition -DriveLetter 'E' -Size (50GB) `
+                | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS `
+                                -NewFileSystemLabel "SQLSys" -Confirm:$false -Force | Out-Null
+    }
+    
+
+    # I drive
+    $VirtualDisk `
+                | New-Partition -DriveLetter 'I' -Size (300GB) `
+                | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS `
+                                -NewFileSystemLabel "SQLLog" -Confirm:$false -Force | Out-Null
+
+    # T drive
+    $VirtualDisk `
+                | New-Partition -DriveLetter 'T' -Size (224GB) `
+                | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS `
+                                -NewFileSystemLabel "TempDB" -Confirm:$false -Force | Out-Null
+
+    # U drive
+    $VirtualDisk `
+                | New-Partition -DriveLetter 'U' -Size (50GB) `
+                | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS `
+                                -NewFileSystemLabel "TempLog" -Confirm:$false -Force | Out-Null
+
+    # F drive
+    $VirtualDisk `
+                | New-Partition -DriveLetter 'F' -UseMaximumSize `
+                | Format-Volume -AllocationUnitSize 65536 -FileSystem NTFS `
+                                -NewFileSystemLabel "SQLData" -Confirm:$false -Force | Out-Null
 
     # Starts the Hardware Detection Service again
     Start-Service -Name ShellHWDetection
 
-    Write-Host "A new volume on Drive F: has successfully been created."
+    Write-Host "Volumes have been successfully configured."
 
 }
 
+# Create new directories if they do not already exist
+$pathsToCreate = @( "E:\SQLSys";
+                    "F:\SQLData";
+                    "F:\SQLData";
+                    "F:\SQLBackup";
+                    "I:\SQLLog";
+                    "T:\SQLTempDB";
+                    "U:\SQLTempLog")
+
+foreach ($path in $pathsToCreate) {
+    if (!(Test-Path $path)) {
+        New-Item -ItemType directory -Path $path | Out-Null
+    }
+}
+
+Write-Host "Drive folders created."
 
 
 ########################################
@@ -170,38 +229,48 @@ Write-Host ".NET Framework 3.5 successfully installed."
 ########################################
 # Install and configure SQL Server
 ########################################
+# Using the following document as a reference for list of parameters:
+# https://msdn.microsoft.com/en-us/library/ms144259(v=sql.120).aspx
 
 # Specify installation parameters
 $myArgList =  '/QS '                                               # Only shows progress, does not accept any user input
 $myArgList += '/ACTION=INSTALL '
 $myArgList += '/IAcceptSQLServerLicenseTerms=1 '                   # Accept the SQL Server license agreement
-$myArgList += '/SQMREPORTING=0 '                                   # Specify that SQL Server feature usage data CANNOT be collected and sent to Microsoft.                            
-            
-$myArgList += '/ERRORREPORTING=0 '                                 # Specify that errors CANNOT be reported to Microsoft.
 $myArgList += '/UPDATEENABLED=0 '                                  # Specify to NOT include product updates.
-$myArgList += '/FEATURES=SQLENGINE,FULLTEXT,AS,RS,SSMS '           # Specifies the Features to install
-            
-$myArgList += '/INSTANCENAME=MSSQLSERVER ' 
-            
+$myArgList += '/ERRORREPORTING=0 '                                 # Specify that errors CANNOT be reported to Microsoft.
+$myArgList += '/SQMREPORTING=0 '                                   # Specify that SQL Server feature usage data CANNOT be collected and sent to Microsoft.                                     
+
+$myArgList += '/FEATURES=SQLENGINE,REPLICATION,FULLTEXT,IS,ADV_SSMS,BC,CONN,SNAC_SDK,SDK '  # Specifies the Features to install        
+
+$myArgList += '/INSTALLSHAREDDIR="E:\SQLSys\Program Files\MicrosoftSQL Server" '            # Specifies a nondefault installation directory for 64-bit shared components.
+$myArgList += '/INSTALLSHAREDWOWDIR="E:\SQLSys\Program Files(x86)\MicrosoftSQL Server" '    # Specifies a nondefault installation directory for 32-bit shared components. 
+$myArgList += '/INSTANCEDIR="E:\SQLSys\Program Files\MicrosoftSQL Server" '                 # Specifies a nondefault installation directory for instance-specific components.
+$myArgList += '/INSTALLSQLDATADIR="E:\SQLSys\Program Files\MicrosoftSQL Server" '           # Specifies the data directory for SQL Server data files.
+
+$myArgList += '/INSTANCENAME=MSSQLSERVER '                         # Specifies a SQL Server instance name.
+      
 $myArgList += '/AGTSVCACCOUNT="NT Service\SQLSERVERAGENT" '        # Agent account name
 $myArgList += '/AGTSVCPASSWORD=W3lc0me0 '                          # Agent account Password
 $myArgList += '/AGTSVCSTARTUPTYPE=Automatic '                      # Auto-start service after installation
 
-$myArgList += '/ASSVCACCOUNT="NT Service\MSSQLServerOLAPService" ' # The name of the account that Analysis Services service runs under
-$myArgList += '/ASSVCPASSWORD=W3lc0me0 '                           # Agent Services service Password
-$myArgList += '/ASSVCSTARTUPTYPE=Automatic '                       # Controls the service starup type setting after the service has been created.
-$myArgList += "/ASSYSADMINACCOUNTS=$ServerName\$LocalAdmin "       # Specifies the list of administrator accounts that need to be provisioned. 
+$myArgList += '/SQLTEMPDBDIR="T:\SQLTempDB" '                      # Specifies the directory for the data files for tempdb.
+$myArgList += '/SQLTEMPDBLOGDIR="U:\SQLTempLog" '                  # Specifies the directory for the log files for tempdb.
+
+$myArgList += '/SQLUSERDBDIR="F:\SQLData" '                        # Specifies the directory for the data files for user databases.
+$myArgList += '/SQLBACKUPDIR="F:\SQLBackup" '                      # Specifies the directory for backup files.
+$myArgList += '/SQLUSERDBLOGDIR="I:\SQLLog" '                      # Specifies the directory for the log files for user databases.
+
+$myArgList += '/ISSVCACCOUNT="NT AUTHORITY\NETWORK SERVICE" '      # Specifies the account for Integration Services.
+$myArgList += '/ISSVCPASSWORD=W3lc0me0 '                           # Specifies the Integration Services password.
 
 $myArgList += '/SQLSVCSTARTUPTYPE=Automatic '                      # Startup type for the SQL Server service
 $myArgList += '/SQLSVCACCOUNT="NT Service\MSSQLSERVER" '           # Account for SQL Server service
 $myArgList += '/SQLSVCPASSWORD=W3lc0me0 '                          # SQL Service Password
 $myArgList += "/SQLSYSADMINACCOUNTS=$ServerName\$LocalAdmin "      # Windows account(s) to provision as SQL Server system administrators.
 
-$myArgList += '/RSSVCACCOUNT="NT Service\ReportServer" '           # Name of the accout for Reporting Services
-$myArgList += '/RSSVCPASSWORD=W3lc0me0 '
-
 $myArgList += '/SECURITYMODE=SQL '                                 # Use SQL for Mixed Mode authentication
-$myArgList += '/SAPWD=W3lc0me0 '         
+$myArgList += '/SAPWD=W3lc0me0 '                                   # Specifies the password for the SQL Serversa account.
+
 
 $myArgList += '/TCPENABLED=1'                                      # Enable TCP/IP Protocol
 
@@ -216,61 +285,53 @@ Write-Host "SQL Server successfully installed."
 ########################################
 
 
-# Create new directories if they do not already exist
-$pathsToCreate = @("F:\SQL_Data";"F:\SQL_Backup";"F:\SQL_Logs";"F:\SQL_TempDB")
-foreach ($path in $pathsToCreate) {
-    if (!(Test-Path $path)) {
-        New-Item -ItemType directory -Path $path | Out-Null
-    }
-}
-
 # Open up firewall ports
 # For Database Engine default instance
-netsh advfirewall firewall add rule name="SQL Instances" dir=in action=allow protocol=TCP localport=1433
-# For Analysis Services
-netsh advfirewall firewall add rule name="SQL Analysis Services" dir=in action=allow protocol=TCP localport=2383
+netsh advfirewall firewall add rule name="SQLServer-TCP-1433" dir=in action=allow protocol=TCP localport=1433
+# For Internal Load Balancer probe port
+netsh advfirewall firewall add rule name="ILBProbePort-TCP-59999" dir=in action=allow protocol=TCP localport=59999
 
 <#
-    Create SQL Query to move the TEMPDB database from its default location to the F: drive
-    Using this guide: http://stackoverflow.com/questions/8423541/how-do-you-run-a-sql-server-query-from-powershell
 
-    Also, for TempDB, create 8 data files (as opposed to the default of 1).
+    Create a SQL query to set the size and number of TempDB files.
+
+    For TempDB, create 8 data files (as opposed to the default of 1).
     Using this as a Best Practices source: http://www.brentozar.com/sql/tempdb-performance-and-configuration/
     
 #>
 $DatabaseName = "master"
 $Query = "
           ALTER DATABASE tempdb
-          MODIFY FILE (name = tempdev, FILENAME = 'F:\SQL_TempDB\tempdb.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+          MODIFY FILE (name = tempdev, FILENAME = 'T:\SQLTempDB\tempdb.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($autogrowTempDBinMB)MB);
           
           ALTER DATABASE tempdb
-          MODIFY FILE (name = templog, FILENAME = 'F:\SQL_TempDB\templog.ldf');
+          MODIFY FILE (name = templog, FILENAME = 'U:\SQLTempLog\templog.ldf');
 
           ALTER DATABASE tempdb
-          ADD FILE (NAME = tempdev2, FILENAME = 'F:\SQL_TempDB\tempdb2.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+          ADD FILE (NAME = tempdev2, FILENAME = 'T:\SQLTempDB\tempdb2.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($autogrowTempDBinMB)MB);
 
           ALTER DATABASE tempdb
-          ADD FILE (NAME = tempdev3, FILENAME = 'F:\SQL_TempDB\tempdb3.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+          ADD FILE (NAME = tempdev3, FILENAME = 'T:\SQLTempDB\tempdb3.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($autogrowTempDBinMB)MB);
 
           ALTER DATABASE tempdb
-          ADD FILE (NAME = tempdev4, FILENAME = 'F:\SQL_TempDB\tempdb4.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+          ADD FILE (NAME = tempdev4, FILENAME = 'T:\SQLTempDB\tempdb4.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($autogrowTempDBinMB)MB);
 
           ALTER DATABASE tempdb
-          ADD FILE (NAME = tempdev5, FILENAME = 'F:\SQL_TempDB\tempdb5.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+          ADD FILE (NAME = tempdev5, FILENAME = 'T:\SQLTempDB\tempdb5.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($autogrowTempDBinMB)MB);
 
           ALTER DATABASE tempdb
-          ADD FILE (NAME = tempdev6, FILENAME = 'F:\SQL_TempDB\tempdb6.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+          ADD FILE (NAME = tempdev6, FILENAME = 'T:\SQLTempDB\tempdb6.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($autogrowTempDBinMB)MB);
 
           ALTER DATABASE tempdb
-          ADD FILE (NAME = tempdev7, FILENAME = 'F:\SQL_TempDB\tempdb7.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+          ADD FILE (NAME = tempdev7, FILENAME = 'T:\SQLTempDB\tempdb7.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($autogrowTempDBinMB)MB);
 
           ALTER DATABASE tempdb
-          ADD FILE (NAME = tempdev8, FILENAME = 'F:\SQL_TempDB\tempdb8.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($percentAutogrowTempDB)%);
+          ADD FILE (NAME = tempdev8, FILENAME = 'T:\SQLTempDB\tempdb8.mdf', SIZE = $($sizeTempDBDataFileMB)MB, FILEGROWTH = $($autogrowTempDBinMB)MB);
          "
 
 # Timeout parameters
-$QueryTimeout = 120
-$ConnectionTimeout = 30
+$QueryTimeout = 600
+$ConnectionTimeout = 120
 
 # Create the connection string and open the connection with the SQL Server instance
 $conn=New-Object System.Data.SqlClient.SQLConnection
@@ -285,6 +346,7 @@ $ds=New-Object System.Data.DataSet
 $da=New-Object System.Data.SqlClient.SqlDataAdapter($cmd)
 [void]$da.fill($ds)
 
+<#
 # Change the default location of the Data, Log, and Backup databases
 # To add Microsoft.SqlServer.Smo objects, following the instructions on the following website: 
 # http://sqlmag.com/powershell/using-sql-server-management-objects-powershell
@@ -294,6 +356,7 @@ $SQLServer.DefaultFile = "F:\SQL_Data"                        # Change the defau
 $SQLServer.DefaultLog = "F:\SQL_Logs"                         # Change the default location of log files
 $SQLServer.BackupDirectory = "F:\SQL_Backup"                  # Change the default location of backup files
 $SQLServer.Alter()                                            # Updates any Server object property changes on the instance of SQL Server. 
+#>
 
 # Close the connection and output any results.
 $conn.Close()
